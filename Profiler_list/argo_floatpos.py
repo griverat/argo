1bc1e8219bea14c154fbe3d18ad62280d1fcbed6
Gerardo Andre Rivera Tello <gerardo_art@me.com>
2019-02-17 21:51:41 -0500

    Remove unused import from dask
    Former-commit-id: 3946c21fd8593ba547688c1bfdb1e3ad841d1b23

--- Profiler_list/argo_floatpos.py
+++ Profiler_list/argo_floatpos.py
@@ -11,7 +11,6 @@ Copyright (c) 2018 Instituto Geofisico del Peru
 
 from dask.distributed import Client, LocalCluster, progress
 from dask import delayed, compute
-from dask.diagnostics import ProgressBar
 import dask.dataframe as dd
 import xarray as xr
 import pandas as pd
