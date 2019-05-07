991f0556535eafd15a970484d26c12eede6ae43f
Gerardo Andre Rivera Tello <gerardo_art@me.com>
2019-05-07 09:03:57 -0500

    Script to extract the last year of an ARGO float trajectory
    Former-commit-id: 7c13de922722a5fe8feeaaada24fe44a2e5f7071

---
+++ Trajectory/argo_filter_traj.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+Created on Mon Apr 22 08:57:36 2019
+Author: Gerardo A. Rivera Tello
+Email: grivera@igp.gob.pe
+-----
+Copyright (c) 2019 Instituto Geofisico del Peru
+-----
+"""
+
+from datetime import datetime
+import pandas as pd
+import numpy as np
+
+
+def filter_date(prof,argodb,days):
+    delta = pd.Timedelta(days,'D')
+    today = pd.to_datetime(datetime.today())
+    st_date = today-delta
+    filtered = argo_db.query('(platfn==@prof)&(date>@st_date)&(lat>-10)&(lat<0)&(lon>270)&(lon<290)').reset_index(drop=True)[['date','lat','lon']]
+    return filtered
+
+
+def build_ts(end_date,tbefore,days):
+    delta = pd.Timedelta(days,'D')
+    tbefore = pd.Timedelta(tbefore,'D')
+    lim_date = end_date - tbefore
+    new_time = end_date
+    date_lims = []
+    date_lims.append(new_time)
+    while new_time > lim_date:
+        new_time -= delta
+        date_lims.append(new_time)
+    date_lims.append(lim_date)
+    return date_lims
+
+
+def group_dates2(filter_db,day_thres):
+    today = pd.to_datetime(datetime.today().replace(hour=0,minute=0,second=0,microsecond=0))
+    date_ranges = build_ts(today, 365, 30)
+    for i in range(len(date_ranges)):
+        dbefore = date_ranges[i]
+        try:
+            dafter = date_ranges[i+1]
+            ix_ = filter_db.query('(date<=@dbefore)&(date>@dafter)').index
+        except:
+            print("Reached final date")
+            ix_ = filter_db.query('date<@dbefore').index
+
+        for ix in ix_:
+            filter_db.loc[ix,'class'] = i
+    return filter_db
+
+
+if __name__ == "__main__":
+    prof_list = np.loadtxt('/home/grivera/GitLab/argo/Output/paita.txt', dtype=int)
+    argo_db = pd.read_csv('/data/users/grivera/ARGO-latlon/latlontemp.txt',parse_dates=[0]).sort_values('date').reset_index(drop=True)
+    argo_filter = filter_date(prof_list,argo_db,365)
+    argo_filter = group_dates2(argo_filter,30)
+    for i in range(1,3):
+        filename = '/data/users/grivera/ARGO-prof/{}-traj{}.txt'.format(prof_list,i)
+        argo_filter.to_csv(filename,header=None,index=None,sep=' ',float_format='%.5f')
