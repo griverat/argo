7818557adbb74e2c2e65d86b003542bc07979e28
Gerardo Rivera Tello <gerardo_art@me.com>
2019-06-19 10:09:35 -0500

    First try to automate the process
    Former-commit-id: a610e71f5c4492cf681e2719aef589ea01ede283

---
+++ auto_argo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#Created on Sun Jun 9 06:07:19 2019
+#Author: Gerardo A. Rivera Tello
+#Email: grivera@igp.gob.pe
+#-----
+#Copyright (c) 2018 Instituto Geofisico del Peru
+#-----
+
+source /home/grivera/miniconda3/etc/profile.d/conda.sh
+conda activate Work
+
+OUTDIR=/data/users/grivera
+SCR_DIR=/home/grivera/GitLab/argo
+
+cd $SCR_DIR || exit
+
+##############################
+#   Build profile database   #
+##############################
+cd "$SCR_DIR/Profiler_list" || exit
+
+python argo_floatpos.py
+python argo_to_json.py
+
+cp Output/* $OUTDIR/ARGO-latlon/
+
+
+##############################
+#    Build Trajectory plot   #
+##############################
+
+cd "$SCR_DIR/Trajectory" || exit
+
+python argo_build_profile.py
+python argo_filter_traj.py
+
+##############################
+#   Build multiple profiles  #
+##############################
+
+cd "$SCR_DIR/Region_data" || exit
+
+sh argo_region_nc.sh
+
