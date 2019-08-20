10bbda0efc40a0cbd589cdfd5ab4c3b6f3b224f4
Gerardo Rivera Tello <gerardo_art@me.com>
2019-08-20 15:14:19 -0500

    Convert to .gif

--- Trajectory/convert_eps.sh
+++ Trajectory/convert_eps.sh
@@ -8,5 +8,6 @@ fi
 
 for line in ${files}; do
     file=$(echo $line | awk -F. '{print $1}')
-    convert -rotate 90 -density 400 $line ${file}.png
+    convert -rotate 90 -density 400 "$line" "${file}.png"
+    convert -rotate 90 "$line" "web/${file}.gif"
 done
