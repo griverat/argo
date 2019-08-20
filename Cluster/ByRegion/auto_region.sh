62851054bb8647b91db6a0c2f8212c63e11328a5
Gerardo Rivera Tello <gerardo_art@me.com>
2019-08-20 09:17:23 -0500

    Back to bash script

---
+++ Cluster/ByRegion/auto_region.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Created on Sun Aug 11 06:16:14 2019
+#Author: Gerardo A. Rivera Tello
+#Email: gerardo_art@me.com
+#-----
+
+python argo_tracker.py 10 'patch' \
+    --reg_list='-2,2;252.5,259' \
+    --reg_list='-2,2;262.5,268.5' \
+    --reg_list='-10,0;270,280' \
+    --plat -15 5 --plon 250 285
+
+python argo_tracker.py 10 'peru' \
+    --reg_list='-20,0;270,290' \
+    --plat -25 5 --plon 260 295
