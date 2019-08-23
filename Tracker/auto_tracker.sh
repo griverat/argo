b4011e7580a88256d610aaef015cafe757e7e7f8
Gerardo Rivera Tello <gerardo_art@me.com>
2019-08-23 17:40:50 -0500

    Add Stratus region

--- Tracker/auto_tracker.sh
+++ Tracker/auto_tracker.sh
@@ -13,3 +13,8 @@ python argo_tracker.py 10 'patch' \
 python argo_tracker.py 10 'peru' \
     --reg_list='-20,0;270,290' \
     --plat -25 5 --plon 260 295
+
+python argo_tracker.py 10 'eqstrat' \
+    --reg_list='-1.5,2;252.5,259' \
+    --reg_list='-22,-16;272.5,280' \
+    --plat -25 5 --plon 250 285
